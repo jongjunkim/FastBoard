ed095115782b26f64d23cd8856e1acd7be4caea4
jongjun <rkdlem196@gmail.com>
2024-01-14 04:16:05 -0500

    Login error fix

--- domain/user/user_crud.py
+++ domain/user/user_crud.py
@@ -16,5 +16,5 @@ async def sign_up(db: AsyncSession, new_user: UserCreate):
     await db.commit()
     
 async def get_user(db: AsyncSession, email: str):
-    result = await db.execute(select(User).where(User.email == email))
-    return result.scalar_one()
+    result = await db.execute(select(User).filter(User.email == email))
+    return result.scalar_one_or_none()
